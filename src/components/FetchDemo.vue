e9b0cbd57cb1a260164068597b4d0cf73b60bff0
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-02-07 15:06:45 +0000

    FetchDemo (feat): copy from bookjs

---
+++ src/components/FetchDemo.vue
@@ -0,0 +1,21 @@
+<template>
+  <template v-if="state.value === 'idle'">
+    <button @click="send({ type: 'FETCH', query: 'users/unique', method: 'POST' })">
+      Get a unique user name
+    </button>
+  </template>
+
+  <template v-else-if="state.value === 'loading'">
+    <div>Fetching...</div>
+  </template>
+
+  <template v-else-if="state.value === 'success'">
+    <div>Success! {{ state.context.results }}</div>
+  </template>
+
+  <template v-else-if="state.value === 'failure'">
+    <p>{{ state.context.error.message }}</p>
+    <button @click="send({ type: 'RETRY', query: 'users/unique', method: 'POST' })">Retry</button>
+  </template>
+</template>
+<script src="./FetchMachine.js"></script>
