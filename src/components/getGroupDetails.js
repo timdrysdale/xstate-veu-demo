8169cc921043e2b614138d8649e81f78fb4531f9
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-02-11 16:02:37 +0000

    getGroupDetails: try promise.all - works

--- src/components/getGroupDetails.js
+++ src/components/getGroupDetails.js
@@ -6,7 +6,7 @@
   });
 }
 */
-
+/*
 export default function (context, event) {
   const group = "g-everyone"; //TODO just for test
 
@@ -26,70 +26,35 @@ export default function (context, event) {
       });
     });
 }
-
-/*
-export default function (context, event) {
-  const groupDetails = {};
-  const promises = [];
-  for (const group in context.groups) {
-    console.log(group);
-    const p = fetch(
-      import.meta.env.VITE_APP_BOOK_SERVER + "/api/v1/groups/" + group,
-      {
-        method: "GET",
-        headers: {
-          Authorization: context.token,
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((details) => {
-        console.log(details);
-        groupDetails[group] = details;
-      });
-    promises.push(p);
-  }
-
-  return promise.all(promises).then(() => {
-    console.log("groupDetails", groupDetails);
-    resolve({ status: "ok", groupDetails: groupDetails });
-  });
-}*/
-
-/*
-    function getData() {
-      const groupDetails = {};
-      const fs = [];
-      const gs = [];
-
-      const checkResult = (res) => (res.ok ? res.json() : Promise.resolve({}));
-
-      for (const group in context.groups) {
-        const f = fetch(
-          import.meta.env.VITE_APP_BOOK_SERVER + "/api/v1/groups/" + group,
-          {
-            method: "GET",
-            headers: {
-              Authorization: context.token,
-            },
-          }
-        ).then(checkResult);
-
-        fs.push(f);
-        gs.push(group);
-      }
-
-      return Promise.all(fs)
-        .then((results) => {
-          results.forEach(function (item, index) {
-            groupDetails[gs[index]] = item;
-          });
-        })
-        .catch((err) => console.error(err));
-    }
-
-    console.log("getGroupDetails");
-    data = getData();
-    console.log("getGroupDetails", data);
-
 */
+
+export default function (context, event) {
+  let group = "g-everyone"; //TODO just for test
+
+  const p0 = fetch(
+    import.meta.env.VITE_APP_BOOK_SERVER + "/api/v1/groups/" + group,
+    {
+      method: "GET",
+      headers: {
+        Authorization: context.token,
+      },
+    }
+  ).then((res) => res.json());
+
+  group = "g-engdes1-lab";
+  const p1 = fetch(
+    import.meta.env.VITE_APP_BOOK_SERVER + "/api/v1/groups/" + group,
+    {
+      method: "GET",
+      headers: {
+        Authorization: context.token,
+      },
+    }
+  ).then((res) => res.json());
+
+  return Promise.all([p0, p1]).then((values) => {
+    return new Promise((resolve, reject) => {
+      resolve({ status: "ok", groupDetails: values });
+    });
+  });
+}
